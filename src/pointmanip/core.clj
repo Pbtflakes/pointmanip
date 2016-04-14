63733e07b5df6261bc2416f2d58a89c3f8fef3d7
foxxtr0T <a1c4s8@gmail.com>
2016-04-14 13:06:03 -0400

    Setup
    Nothing but the leiningen files here at the moment.

---
+++ src/pointmanip/core.clj
@@ -0,0 +1,7 @@
+(ns pointmanip.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/pointmanip/core_test.clj
@@ -0,0 +1,7 @@
+(ns pointmanip.core-test
+  (:require [clojure.test :refer :all]
+            [pointmanip.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
